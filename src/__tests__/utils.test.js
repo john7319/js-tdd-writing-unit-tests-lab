c669667a4018446e83477f148b476ea0310e01a2
john7319 <jonniekaras@gmail.com>
2024-05-03 22:33:44 +0300

    Added tests to check for both upper and lower and to check for a palindrome

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,24 @@
 // Your tests here
+import { isPalindrome, hasBoth } from "../utils"
+
+describe ("isPalindrome", () => {
+    it("Returns true if a word is a palindrome and false if not", () => {
+        // Checks for palindrome 
+        const word = "mom";
+        const palindrome = isPalindrome(word);
+        expect(palindrome).toBe(true);
+        // checks for non palindrome
+        const nonWord = "how"
+        const nonPalindrome = isPalindrome(nonWord)
+        expect(nonPalindrome).toBe(false)
+
+    });
+});
+
+describe ("hasBoth", () => {
+    it("returns true if a word has both upper and lowercase letters", () => {
+        const hasUpperLower = "Hey"
+        const checkHasBoth = hasBoth(hasUpperLower)
+        expect(checkHasBoth).toBe(true)
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,23 @@
 // Your code here
+export function isPalindrome(word){
+
+    const newword = word.toLowerCase();
+    const wordCheck = newword.split("").reverse().join("");
+    if (wordCheck === word){
+        return true;
+    }else{
+        return false;
+    }
+
+
+}
+export function hasBoth(str) {
+    const lowerCaseRegex = /[a-z]/;
+    const upperCaseRegex = /[A-Z]/;
+    return lowerCaseRegex.test(str) && upperCaseRegex.test(str);
+}
+hasBoth("Hallo")
+
+isPalindrome(" ")
+isPalindrome("mom")
+isPalindrome("cow")
